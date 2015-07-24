e7a5bbfb4bfa8e616aa2dcedbb26ffc9c4658453
kmdouglass <kyle.m.douglass@gmail.com>
2015-07-24 11:58:18 +0200

    _parseFolder now searches a directory tree for .dat files.

--- smclusters/smclusters.py
+++ smclusters/smclusters.py
@@ -30,18 +30,18 @@ class smclusters:
     def __init__(self,
                  folder = '.',
                  algorithm = 'DBSCAN',
-                 options = {'min_samples': 8, 'eps': 65},
+                 options = {'min_samples': 50, 'eps': 20},
                  usecols = (0,1)):
         """Find the localization data and perform the clustering algorithm.
         
         Parameters
         ----------
         folder    : str  (optional, default: '.')
-            The folder containing the localization data files. Defaults to the
-            current Python interpretor working directory.
+            The folder containing the localization data directories. Defaults
+            to the current Python interpretor working directory.
         algorithm : str  (optional, default: 'DBSCAN')
             The clustering algorithm run on the data
-        options   : dict (optional, default: {k: 8, eps: 65})
+        options   : dict (optional, default: {min_samples: 8, eps: 65})
             The input parameters used by the clustering algorithm. If the
             values of the dict are lists of values, then the clustering routine
             is run for each set of values in corresponding positions in the
@@ -50,8 +50,10 @@ class smclusters:
             The columns in the localization data files to use for clustering.
             Typically, these are the x-, y-, and possibly z-coordinates.
         """
-        self._usecols = usecols
-        self._folder = Path(folder)
+        self._usecols   = usecols
+        self._folder    = Path(folder)
+        self._algorithm = algorithm
+        self._options   = options
         self._locResultFiles = self._parseFolder()
         
         self.cData = None
@@ -59,20 +61,20 @@ class smclusters:
 
                 
     def _parseFolder(self, fileSuffix = '.dat'):
-        """Finds all localization data files in a directory
+        """Finds all localization data files in a directory tree.
         
         Parameters
         ----------
         fileSuffix      : str (optional, default: '.dat')
             Suffix for localization result files. This must be unique to
-            localization result files only within the given folder.
+            files containing localization data.
         
         Returns
         -------
         locResultFiles  : list of Path
-            A list of all the localization data files in a directory
+            A list of all the localization data files in a directory tree
         """
-        locResultFilesGen = self._folder.glob('*{:s}'.format(fileSuffix))
+        locResultFilesGen = self._folder.glob('**/*{:s}'.format(fileSuffix))
         locResultFiles    = sorted(locResultFilesGen)
         
         return locResultFiles
@@ -80,26 +82,39 @@ class smclusters:
     def fit(self):
         """Run the clustering algorithm on the localization data
         
-        doClustering runs the specified clustering algorithm on all
-        localization data files in the given folder. Statistics for each
-        cluster are then computed.
+        fit runs the specified clustering algorithm on all localization
+        datasets in the given directory tree. Statistics for each cluster are
+        then computed after each clustering.
         """
-                
-        #[print(x.resolve()) for x in self._locResultFiles]
+        
+        # Unpack the clustering algorithm parameters
+        if self._algorithm is 'DBSCAN':
+            cluster     = DBSCAN().fit
+            min_samples = self._options['min_samples']
+            eps         = self._options['eps']
         
         for currFile in self._locResultFiles:
+            print('Currently processing {:s}...'.format(currFile.parts[-2]))            
+            
             filePath = str(currFile.resolve())
             
             # Import the localization data into a NumPy array
             currData = np.loadtxt(filePath, delimiter = ',', skiprows = 1, usecols = self._usecols)
             
             # Perform clustering on the localization data
-            db = DBSCAN().fit(currData)
+            db = cluster(currData)
             
             # Extract the cluster labels corresponding to each localization
             clusterLabels = db.labels_
             
             # Save the labels to a csv file
+            savePath = str(currFile.parent) + '/clusterLabels.csv'
+            np.savetxt(savePath, clusterLabels, delimiter = ',', fmt = '%i')
+            
+    def computeClusterStats(self):
+        """Computes the statistics of each cluster.
+        """
+        pass
         
     def saveData(self):
         """Saves the cluster- and meta-data to the disk.
