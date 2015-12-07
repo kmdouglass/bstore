4f1301ebceeabeedb9c9e364470299d6c9be09ac
kmdouglass <kyle.m.douglass@gmail.com>
2015-12-07 16:50:32 +0100

    Merge now averages the sigma values--needs testing.
    Former-commit-id: de9af24fd01361aa2243b000a41570fda3f85a9a

--- DataSTORM/processors.py
+++ DataSTORM/processors.py
@@ -1016,7 +1016,8 @@ class Merge:
         tempResultsMisc        = particleGroups.agg({'loglikelihood' : 'mean',
                                                      'frame'         : 'min',
                                                      'photons'       : 'sum',
-                                                     'bg'            : 'sum'})
+                                                     'bg'            : 'sum',
+                                                     'sigma'         : 'mean'})
         tempResultsLength      = pd.Series(particleGroups.size())
 
         # Rename the series
