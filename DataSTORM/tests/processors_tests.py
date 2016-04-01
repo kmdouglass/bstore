7253d407e2c78f1ff8bd82389bd4f539b359895a
kmdouglass <kyle.m.douglass@gmail.com>
2016-04-01 18:09:28 +0200

    Added unit test for dropping fiducials.
    Former-commit-id: 5238c9cdd6b9de14a4f1edad8ee121e3b3b2854d

--- DataSTORM/tests/processors_tests.py
+++ DataSTORM/tests/processors_tests.py
@@ -4,13 +4,13 @@ import pandas as pd
 import numpy as np
 import sys
 
+# Load localization + fiducial ground truth test set
+locs = pd.read_csv('test_files/test_localizations_with_fiducials.csv')
+
 def test_DriftCorrection():
     """Drift correction is properly applied to all localizations.
     
     """
-    # Load localization + fiducial ground truth test set
-    locs = pd.read_csv('test_files/test_localizations_with_fiducials.csv')
-    
     # Create the drift corrector
     dc = proc.FiducialDriftCorrect(minFracFiducialLength = 0.5,
                                    neighborRadius        = 150,
@@ -50,6 +50,75 @@ def test_DriftCorrection():
     # localizations after the localizations belonging to
     # fiducials are removed.
     assert_equal(corrLocs.shape[0], 20000)
+    
+    # Was the correct drift trajectory applied? The original locs
+    # should be in x + dx and y + dy of corrdf
+    # round() avoids rounding errors when making comparisons
+    checkx = (corrLocs['x [nm]'] + corrLocs['dx [nm]']).round(2).isin(
+                                           locs['x [nm]'].round(2).as_matrix())
+    checky = (corrLocs['y [nm]'] + corrLocs['dy [nm]']).round(2).isin(
+                                           locs['y [nm]'].round(2).as_matrix())
+    ok_(checkx.all())
+    ok_(checky.all())
+    
+    # dx and dy should equal the avgSpline
+    fidTraj_x = corrLocs[['dx [nm]', 'frame']].sort_values(
+              'frame').drop_duplicates('frame')['dx [nm]'].round(2).as_matrix()
+    fidTraj_y = corrLocs[['dy [nm]', 'frame']].sort_values(
+              'frame').drop_duplicates('frame')['dy [nm]'].round(2).as_matrix()
+    spline_x  = dc.avgSpline['xS'].round(2).as_matrix()
+    spline_y  = dc.avgSpline['yS'].round(2).as_matrix()
+    ok_(all(fidTraj_x == spline_x))
+    ok_(all(fidTraj_y == spline_y))
+    
+def test_DriftCorrection_dropTrajectories():
+    """Drift correction works after a trajectory is dropped.
+    
+    """    
+    # Create the drift corrector
+    dc = proc.FiducialDriftCorrect(minFracFiducialLength = 0.5,
+                                   neighborRadius        = 150,
+                                   smoothingWindowSize   = 800,
+                                   smoothingFilterSize   = 200,
+                                   interactiveSearch     = False,
+                                   doFiducialSearch      = False,
+                                   noLinking             = True,
+                                   noClustering          = False,
+                                   removeFiducials       = True)
+                                   
+    # Tell the drift corrector where the fiducials are.
+    # Normally, these methods are not directly access by users; this is why
+    # we need to handle renaming of columns.
+    dc.searchRegions = [
+        {'xMin' : 730,
+         'xMax' : 870,
+         'yMin' : 730,
+         'yMax' : 820},
+         {'xMin' : 1400,
+          'xMax' : 1600,
+          'yMin' : 1400,
+          'yMax' : 1600}
+         ]
+    locs.rename(columns = {'x [nm]' : 'x', 'y [nm]' : 'y'}, inplace = True)
+    fidRegions = dc.reduceSearchArea(locs)
+    dc.detectFiducials(fidRegions)
+    locs.rename(columns = {'x' : 'x [nm]', 'y' : 'y [nm]'}, inplace = True)
+    
+    # Did we find the trajectories?
+    assert_equal(len(dc.fiducialTrajectories), 2)
+    
+    # Correct the localizations
+    corrLocs = dc(locs)
+    assert_equal(corrLocs.shape[0], 20000)
+    
+    # Drop second trajectory (index = 1)
+    dc.dropTrajectories([1])
+    assert_equal(len(dc.fiducialTrajectories), 1)
+    
+    # Recorrect the localizations
+    corrLocs = dc(locs)
+    # Since we didn't drop a trajectory, there should be 30000 locs now
+    assert_equal(corrLocs.shape[0], 30000)
     
     # Was the correct drift trajectory applied? The original locs
     # should be in x + dx and y + dy of corrdf
