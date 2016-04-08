a024a980788a5495cb4932958f22c0b478136bfa
kmdouglass <kyle.m.douglass@gmail.com>
2016-04-08 09:30:58 +0200

    Updated version in setup.py
    Former-commit-id: 726ac21544d8fd9f5261383c13d09a26ebcdbbeb

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url'             : 'https://github.com/kmdouglass/DataSTORM',
     'download_url'    : 'https://github.com/kmdouglass/DataSTORM',
     'author_email'    : 'kyle.m.douglass@gmail.com',
-    'version'         : '0.1a',
+    'version'         : '0.1.0',
     'install_requires': ['nose',
                          'scikit-learn',
                          'pandas',
