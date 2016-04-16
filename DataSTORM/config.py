82396dbfa21c9193ca6f53e0efb121c3c35bf634
kmdouglass <kyle.m.douglass@gmail.com>
2016-04-16 11:06:40 +0200

    Update git hash tracking.
    Former-commit-id: da700bd0faed88310633ece0871c6af789c17c18

--- DataSTORM/config.py
+++ DataSTORM/config.py
@@ -1,3 +1,3 @@
-__DataSTORM_Version__ = 'v0.1.0a-rev42274c6'
+__DataSTORM_Version__ = 'v0.1.0a-9d57393'
 __HDF_AtomID_Prefix__ = 'SMLM_'
 __HDF_Metadata_Prefix__ = __HDF_AtomID_Prefix__ + 'Metadata_'
