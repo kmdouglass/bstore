bef54e6be813e744cb6205c5e61baa5e263f8e6b
kmdouglass <kyle.m.douglass@gmail.com>
2016-04-16 11:04:56 +0200

    Updated config.py utility to only write the version in the first line.
    Former-commit-id: 42274c6b2f3252140cfa5dcdc1b2f5643f6125dd

--- DataSTORM/config.py
+++ DataSTORM/config.py
@@ -1,3 +1,3 @@
-__DataSTORM_Version__     = 'v0.1.0a-revbc214dd'
-__HDF_AtomID_Prefix__     = 'SMLM_'
-__HDF_Metadata_Prefix__   = __HDF_AtomID_Prefix__ + 'Metadata_'
+__DataSTORM_Version__ = 'v0.1.0a-reveae5d0e'
+__HDF_AtomID_Prefix__ = 'SMLM_'
+__HDF_Metadata_Prefix__ = __HDF_AtomID_Prefix__ + 'Metadata_'
--- utils/commit_version.sh
+++ utils/commit_version.sh
@@ -2,6 +2,6 @@
 
 CURRENT_VERSION="v0.1.0a-rev"
 HASH="$(git log --pretty=format:'%h' -n 1)"
-echo "__DataSTORM_Version__ = '$CURRENT_VERSION${HASH}'" > 'DataSTORM/config.py'
+sed -i "1s/.*/__DataSTORM_Version__ = \'$CURRENT_VERSION${HASH}\'/" DataSTORM/config.py
 git add DataSTORM/config.py
 git commit -m "Update git hash tracking."
