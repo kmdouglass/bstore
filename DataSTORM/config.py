98bb840b2ec7defa7c645e02b0aac22d6b6d44e4
kmdouglass <kyle.m.douglass@gmail.com>
2016-05-21 12:49:55 +0200

    Update git hash tracking.

--- DataSTORM/config.py
+++ DataSTORM/config.py
@@ -1,4 +1,4 @@
-__DataSTORM_Version__ = 'v0.1.0a-c7e9cc6'
+__DataSTORM_Version__ = 'v0.1.0a-a934554'
 __HDF_AtomID_Prefix__ = 'SMLM_'
 __HDF_Metadata_Prefix__ = __HDF_AtomID_Prefix__ + 'Metadata_'
 channelIdentifier = {'A488' : 'AlexaFluor 488',
