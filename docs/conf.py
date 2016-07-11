831b0ee779b407a01da60c601a37a0825fab01c5
kmdouglass <kyle.m.douglass@gmail.com>
2016-07-11 17:50:44 +0200

    Updated Sphinx conf.py to use Napolean for parsing Numpy docstrings.

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ import os
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
