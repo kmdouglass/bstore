c4fefbb46eab74aa391d51ddf7e9cb48e86cd086
kmdouglass <kyle.m.douglass@gmail.com>
2016-10-11 16:45:55 +0200

    Fixed bug in __init__.py that did not create a plugin directory.

--- bstore/__init__.py
+++ bstore/__init__.py
@@ -3,8 +3,11 @@ import sys, os
 
 __version__ = cfg.__bstore_Version__
 
-# Add the configuration and plugin directory to the Python path
+# Create the configuration and plugin directories to the Python path
+# Add the configuration directory to the Python path
 cdir = os.path.expanduser(os.path.join(*cfg.__Custom_Dir__))
+pdir = os.path.expanduser(os.path.join(*cfg.__Plugin_Dir__))
 os.makedirs(cdir, exist_ok = True)
+os.makedirs(pdir, exist_ok = True)
 sys.path.append(cdir)
 del(sys, os)
